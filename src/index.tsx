40ce73bec302381ce59a757d250e49663c020a5c
Filipp Shumakov <shum8929@gmail.com>
2021-07-22 16:16:15 +0300

    hotfix: remove temp tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
-import './wdyr';
-import { hijackEffectHook } from 'stop-runaway-react-effects';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  hijackEffectHook('useLayoutEffect', { callCount: 5, timeLimit: 1000 });
-}
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
